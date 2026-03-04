f5530f62fa2c352aa836d6b8275860b3ed9c47be
Mohd Aasif <aasifmohd1385@gmail.com>
2026-03-04 12:45:33 +0530

    initial commit

---
+++ src/main/java/com/example/zenVault/ZenVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.zenVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenVaultApplication.class, args);
+	}
+
+}
