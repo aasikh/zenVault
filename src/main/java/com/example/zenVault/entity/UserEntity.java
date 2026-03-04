86b77b2d3489b596cf89b7b2801a64a4601fdaba
Sairam <sairamgudelliwar4@gmail.com>
2026-03-04 15:14:03 +0530

    i have created user table in entity

--- src/main/java/com/example/zenVault/entity/UserEntity.java
+++ src/main/java/com/example/zenVault/entity/UserEntity.java
@@ -1,4 +1,23 @@
 package com.example.zenVault.entity;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Users")
 public class UserEntity {
+    private Long ID;
+
+    private String name;
+
+    private String Number;
+
+    private String Email;
+
+    private String password;
+
+    private Enum role;
+
+    private Enum status;
+
+    private LocalDate creatAt;
 }
