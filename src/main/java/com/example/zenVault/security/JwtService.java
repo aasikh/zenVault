6da1029a61c8a2cd76fbadc54abb751bdee8da1e
Mohd Aasif <aasifmohd1385@gmail.com>
2026-03-05 14:52:22 +0530

    add feature for validate token

--- src/main/java/com/example/zenVault/security/JwtService.java
+++ src/main/java/com/example/zenVault/security/JwtService.java
@@ -1,6 +1,7 @@
 package com.example.zenVault.security;
 
 
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -20,6 +21,18 @@ public class JwtService {
                 .signWith(key)
                 .compact();
         return token;
+    }
+    public boolean validateToken(String token) {
+    try{
+    Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token);
+    return true;
+}catch(Exception e){
+    return false;
+}
+
     }
 
 }
